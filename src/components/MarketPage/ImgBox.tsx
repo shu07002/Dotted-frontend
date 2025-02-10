93670aeba3f3d09985efc64f68620ad1db0cf2d8
shu07002 <shu07002@gmail.com>
2025-02-10 20:34:53 +0900

    feat: Market 글쓰기 페이지 컴포넌트 분리

---
+++ src/components/MarketPage/ImgBox.tsx
@@ -0,0 +1,166 @@
+import React from 'react';
+import styled from 'styled-components';
+import Plus from '@/assets/svg/MarketPage/Plus.svg?react';
+
+interface ImgBoxProps {
+  previews: (string | null)[];
+  imgFileRef: React.RefObject<HTMLInputElement>;
+  handleDeleteImage: (index: number) => void;
+  handleFileChange: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => void;
+}
+
+export default function ImgBox({
+  previews,
+  imgFileRef,
+  handleDeleteImage,
+  handleFileChange
+}: ImgBoxProps) {
+  return (
+    <ImgBoxContainer>
+      <ImgBoxWrapper>
+        {previews.map((preview, index) => (
+          <div key={index}>
+            {preview ? (
+              <figure onClick={() => handleDeleteImage(index)}>
+                <img src={preview} alt={`preview-${index}`} />
+              </figure>
+            ) : previews.length < 6 ? (
+              <label htmlFor={`file-${index}`}>
+                <EachImage>
+                  <span>
+                    <Plus />
+                  </span>
+                  <span>{previews.length - 1}/5</span>
+                </EachImage>
+              </label>
+            ) : null}
+            <input
+              type="file"
+              accept="image/*"
+              id={`file-${index}`}
+              ref={imgFileRef}
+              onChange={(e) => handleFileChange(e, index)}
+              style={{ display: 'none' }}
+            />
+          </div>
+        ))}
+      </ImgBoxWrapper>
+    </ImgBoxContainer>
+  );
+}
+
+const ImgBoxContainer = styled.div`
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ImgBoxWrapper = styled.div`
+  width: 100%;
+  margin-top: 2rem;
+  display: flex;
+  gap: 1rem;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: gray;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: var(--primary-color);
+    border-radius: 10px;
+  }
+
+  /* 스크롤바 핸들 호버 시 */
+  &::-webkit-scrollbar-thumb:hover {
+    background: rgba(178, 31, 124, 0.7);
+  }
+
+  > div {
+    position: relative;
+    > figure {
+      img {
+        object-fit: cover;
+        width: 15rem;
+        height: 15rem;
+      }
+
+      &:hover img {
+        filter: brightness(0.7); /* 이미지만 흐려짐 */
+      }
+
+      &::after {
+        content: 'X'; /* 표시할 텍스트 */
+        color: white;
+        font-size: 2rem;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: rgba(0, 0, 0, 0.6); /* 반투명 배경 */
+        border-radius: 50%;
+        width: 4rem;
+        height: 4rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        opacity: 0; /* 기본 숨김 */
+        transition: opacity 0.3s ease; /* 부드러운 전환 효과 */
+        z-index: 1; /* X 버튼이 이미지 위에 오도록 설정 */
+      }
+
+      /* 호버 시 X 표시 나타나기 */
+      &:hover::after {
+        opacity: 1;
+      }
+
+      /* X 버튼에 흐림 효과가 적용되지 않도록 */
+      &:hover::after {
+        filter: none; /* X 버튼은 흐려지지 않음 */
+      }
+    }
+  }
+`;
+
+const EachImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+  align-items: center;
+  font-size: 2rem;
+  cursor: pointer;
+  border-radius: 0.5rem;
+  border: 1px solid ${({ theme }) => theme.colors.gray400};
+  background: ${({ theme }) => theme.colors.backgroundLayer1};
+
+  &:hover {
+    background: rgba(217, 217, 217, 0.7);
+  }
+  width: 15rem;
+  height: 15rem;
+  margin-bottom: 0.5rem;
+
+  > span {
+    &:first-child {
+      font-size: 7rem;
+    }
+    color: ${({ theme }) => theme.colors.gray400};
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 2.4rem; /* 150% */
+    letter-spacing: -0.032rem;
+  }
+`;
