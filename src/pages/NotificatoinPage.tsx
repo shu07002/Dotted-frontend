c422fcad500a6cd1554c256d693ff130bf20734d
shu07002 <shu07002@gmail.com>
2025-02-27 03:55:17 +0900

    fix: 메인 페이지 커뮤 글 5개 나오게

--- src/pages/NotificatoinPage.tsx
+++ src/pages/NotificatoinPage.tsx
@@ -30,19 +30,18 @@ export default function NotificatoinPage() {
   const [notice, setNotice] = useState<AllInfoNotification | null>(null);
 
   useEffect(() => {
-    let accessToken = localStorage.getItem('accessToken');
-    if (!accessToken) return;
-    const EventSourceConstructor = EventSourcePolyfill || NativeEventSource;
-    const headers = { Authorization: `Bearer ${accessToken}` };
-
-    console.log(`${import.meta.env.VITE_API_DOMAIN}/api/notification/stream`);
-    const evtSource = new EventSourceConstructor(
-      `${import.meta.env.VITE_API_DOMAIN}/api/notification/stream`,
-      { headers: headers, withCredentials: true }
-    );
-
     const runSSE = async () => {
       // 토큰이 없는 경우 연결하지 않음
+      let accessToken = localStorage.getItem('accessToken');
+      if (!accessToken) return;
+      const EventSourceConstructor = EventSourcePolyfill || NativeEventSource;
+      const headers = { Authorization: `Bearer ${accessToken}` };
+
+      console.log(`${import.meta.env.VITE_API_DOMAIN}/api/notification/stream`);
+      const evtSource = new EventSourceConstructor(
+        `${import.meta.env.VITE_API_DOMAIN}/api/notification/stream`,
+        { headers: headers, withCredentials: true }
+      );
 
       // 토큰 만료 여부 체크, 만료되었다면 갱신 시도
       if (isTokenExpired(accessToken)) {
@@ -93,11 +92,6 @@ export default function NotificatoinPage() {
     };
 
     runSSE();
-
-    return () => {
-      evtSource.close();
-      console.log('SSE 연결 종료');
-    };
   }, []);
 
   useEffect(() => {
